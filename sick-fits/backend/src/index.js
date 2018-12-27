e9fd35fa630087d3949e3dc43a26347a69069cb5
Angus Clark <angus.c@mmtdigital.co.uk>
2018-12-27 15:18:23 +0000

    finish video 13: create db and server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env'})
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+// TODO Use express to handle cookies (JWT)
+// TODO Use express to populate current users
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URLS
+  },
+}, deets => {
+  console.info(`Server is now running on port http://localhost:${deets.port}`)
+})
